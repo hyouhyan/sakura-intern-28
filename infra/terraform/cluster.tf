2cabec84ec1cc090e0ec29da3ed3a8259f79688c
hyouhyan <hyouhyan@hyouhyan.com>
2026-08-18 14:19:36 +0900

    feat(wip): クラスタ作成
    pricipal idの直書きあり

--- infra/terraform/cluster.tf
+++ infra/terraform/cluster.tf
@@ -1,3 +1,18 @@
 ########################################
 # クラスタ
 ########################################
+resource "sakura_apprun_dedicated_cluster" "example" {
+  name = var.cluster_name
+  service_principal_id = "113801849617"
+  lets_encrypt_email   = var.cluster_lets_encrypt_email
+  ports = [
+    {
+      port     = 80
+      protocol = "http"
+    },
+    {
+      port     = 443
+      protocol = "https"
+    }
+  ]
+}
